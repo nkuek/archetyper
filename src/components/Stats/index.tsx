c4ab6382a589749913012dd2cf71f71df6fab142
Nick Kuek <nkuek1@gmail.com>
2022-01-26 18:27:22 -0500

    clean up recharts

--- src/components/Stats/index.tsx
+++ src/components/Stats/index.tsx
@@ -5,15 +5,35 @@ import { Box } from '@mui/system';
 import randomizeWords from 'words';
 import ReplayIcon from '@mui/icons-material/Replay';
 import {
-  LineChart,
   Line,
   ResponsiveContainer,
   XAxis,
   YAxis,
   Tooltip,
   CartesianGrid,
+  ComposedChart,
+  Scatter,
+  Layer,
 } from 'recharts';
 import { Tooltip as MuiTooltip } from '@mui/material';
+
+const CustomX = (props: any) => {
+  if (!props.payload.errors) return null;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="red"
+      className="bi bi-x"
+      viewBox="0 0 16 16"
+      x={props.x - 4}
+      y={props.y + -4}
+    >
+      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+    </svg>
+  );
+};
 
 const CustomTooltip = (props: any) => {
   const { payload } = props;
@@ -143,20 +163,42 @@ const Stats = () => {
         </MuiTooltip>
       </Container>
       <ResponsiveContainer width={'100%'} height={250}>
-        <LineChart margin={{ right: 20 }} data={wpmData}>
+        <ComposedChart margin={{ right: 20 }} data={wpmData}>
           <CartesianGrid />
           <XAxis dataKey="wordNum" />
           <YAxis
+            yAxisId="left"
             label={{
               value: 'words per minute',
               angle: -90,
               dx: -15,
             }}
             dataKey="wpm"
+            type="number"
+          />
+          <YAxis
+            yAxisId="right"
+            orientation="right"
+            label={{
+              value: 'errors',
+              angle: 90,
+              dx: 15,
+            }}
+            dataKey="errors"
+            domain={[0, 'dataMax + 1']}
+            allowDecimals={false}
+            type="number"
           />
           <Tooltip content={<CustomTooltip />} />
-          <Line type="monotone" dataKey={'wpm'} />
-        </LineChart>
+          <Line yAxisId="left" type="monotone" dataKey="wpm" dot={false} />
+          <Scatter
+            yAxisId="right"
+            type="monotone"
+            dataKey="errors"
+            fill="red"
+            shape={<CustomX />}
+          />
+        </ComposedChart>
       </ResponsiveContainer>
       <Container
         sx={{
