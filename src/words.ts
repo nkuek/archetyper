8c251c0ae371f1e4de400d5611cc6365a68c2a92
Nick Kuek <nkuek1@gmail.com>
2022-02-12 16:01:22 -0500

    reverted max words back to 50

--- src/words.ts
+++ src/words.ts
@@ -208,7 +208,7 @@ const numbersList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const specialCharsList = ['"', '!', '?', ':', ';', '/', '(', '-', "'"];
 
 const maxNumLength = 6;
-export const maxWords = 20;
+export const maxWords = 50;
 
 const randomizeWords = (settings: ISettings) => {
   const { capitalChars, specialChars, numbers } = settings;
