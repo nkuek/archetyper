3ab679766ee8799297fd4090e5ba2285b04540ac
Nick Kuek <nkuek1@gmail.com>
2022-02-14 16:28:13 -0500

    split word count options into separate component

---
+++ src/components/WordOptions/WordCountOptions.tsx
@@ -0,0 +1,75 @@
+import React, { useContext, useMemo } from 'react';
+import { useReset } from 'hooks';
+import { ThemeContext, WordContext, WordListContext } from 'providers';
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+
+const options = [10, 25, 50, 'endless'] as const;
+const WordCountOptions = () => {
+  const reset = useReset(true);
+
+  const { theme } = useContext(ThemeContext);
+
+  const textColor = useMemo(() => theme.wordsContrast || theme.words, [theme]);
+
+  const { settings } = useContext(WordContext);
+  const { setWordCount, wordCount } = useContext(WordListContext);
+
+  return (
+    <div style={{ display: 'flex', padding: 0, alignItems: 'center' }}>
+      {options.map((option, idx) => (
+        <div
+          style={{
+            display: 'flex',
+            opacity: settings.quotes ? 0 : 1,
+            visibility: settings.quotes ? 'hidden' : 'visible',
+            transition: 'opacity 200ms ease-in-out, visibility 200ms linear',
+          }}
+          key={'box' + idx}
+        >
+          <Box
+            sx={{
+              padding: '0em .5em',
+              cursor: 'pointer',
+              color: option === wordCount ? theme.currentWord : textColor,
+              opacity: option === wordCount ? 1 : 0.6,
+              transition: 'opacity 200ms ease-in-out',
+              '&:hover': {
+                opacity: 1,
+              },
+            }}
+            key={`${option}${idx}`}
+            onClick={(e) => {
+              e.stopPropagation();
+              setWordCount(option);
+              reset(e);
+              localStorage.setItem('typer-word-count', JSON.stringify(option));
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: option === 'endless' ? '1.5rem' : '1rem',
+                display: 'flex',
+                alignItems: 'center',
+                lineHeight: 'normal',
+                height: 20,
+                boxSizing: 'border-box',
+                fontWeight: 'bold',
+                borderBottom: option === wordCount ? '1px solid' : 'none',
+                borderColor: 'inherit',
+                marginBottom: option !== wordCount ? '1px' : 0,
+              }}
+            >
+              {option !== 'endless' ? option : String.fromCharCode(8734)}
+            </Typography>
+          </Box>
+          <Box sx={{ color: textColor }} key={'spacer' + idx}>
+            {idx !== options.length - 1 && '/'}
+          </Box>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default WordCountOptions;
--- src/components/WordOptions/index.tsx
+++ src/components/WordOptions/index.tsx
@@ -1,22 +1,21 @@
-import { useMemo, useContext } from 'react';
+import { useMemo, useContext, useState } from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import { ThemeContext, WordListContext, WordContext } from 'providers';
-import { useReset } from 'hooks';
+import { ThemeContext, WordContext } from 'providers';
+import WordCountOptions from './WordCountOptions';
 import { Typography } from '@mui/material';
 
-const options = [10, 25, 50, 'endless'] as const;
+const categories = ['words', 'quotes', 'timed'] as const;
 
 const WordOptions = () => {
-  const { setWordCount, wordCount } = useContext(WordListContext);
   const values = useContext(WordContext);
   const { wpm, settings } = values;
-
-  const reset = useReset(true);
 
   const { theme } = useContext(ThemeContext);
 
   const textColor = useMemo(() => theme.wordsContrast || theme.words, [theme]);
+
+  const [category, setCategory] = useState();
 
   return (
     <Container
@@ -24,57 +23,17 @@ const WordOptions = () => {
         display: 'flex',
         justifyContent: 'space-between',
         marginBottom: '.5em',
-        height: 25,
       }}
     >
-      <div style={{ display: 'flex', padding: 0 }}>
-        {options.map((option, idx) => (
-          <div
-            style={{
-              display: 'flex',
-              opacity: settings.quotes ? 0 : 1,
-              visibility: settings.quotes ? 'hidden' : 'visible',
-              transition: 'opacity 200ms ease-in-out, visibility 200ms linear',
-            }}
-            key={'box' + idx}
-          >
-            <Box
-              sx={{
-                padding: '0em .5em',
-                cursor: 'pointer',
-                color: textColor,
-              }}
-              key={`${option}${idx}`}
-              onClick={(e) => {
-                e.stopPropagation();
-                setWordCount(option);
-                reset(e);
-                localStorage.setItem(
-                  'typer-word-count',
-                  JSON.stringify(option)
-                );
-              }}
-            >
-              <Typography
-                sx={{
-                  borderBottom:
-                    option === wordCount ? `1px solid ${textColor}` : 'none',
-                  fontSize: option === 'endless' ? '1.5rem' : '1rem',
-                  display: 'flex',
-                  alignItems: 'center',
-                  height: '100%',
-                }}
-              >
-                {option !== 'endless' ? option : String.fromCharCode(8734)}
-              </Typography>
-            </Box>
-            <Box sx={{ color: textColor }} key={'spacer' + idx}>
-              {idx !== options.length - 1 && '/'}
-            </Box>
-          </div>
-        ))}
-      </div>
-      <div style={{ display: 'flex' }}>
+      <Box>
+        <Box>
+          {categories.map((category) => (
+            <Typography key={category}>{category}</Typography>
+          ))}
+        </Box>
+        <WordCountOptions />
+      </Box>
+      <div style={{ display: 'flex', alignSelf: 'flex-end' }}>
         <Box sx={{ color: textColor, marginRight: '.25em' }}>{'wpm: '}</Box>
         <Box sx={{ color: textColor }}>{wpm.net || ''}</Box>
       </div>
