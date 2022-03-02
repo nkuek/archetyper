3532644703c74a3a6d33558d384490a878c604c8
Nick Kuek <nkuek1@gmail.com>
2022-03-02 01:17:31 -0500

    better production build

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
 const express = require('express');
-const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
+const compression = require('compression');
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
+
+app.use(compression());
+// app.use(favicon('../frontend/build/favicon.ico'));
+
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join('../frontend', 'build')));
+
 app.get('/ping', function (req, res) {
- return res.send('pong');
+  return res.send('pong');
 });
+
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
 });
+
 app.listen(port);
