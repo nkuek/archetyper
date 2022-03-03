f82b5e2df080197eeab3a3615bbade0e5ee4d644
Nick Kuek <nkuek1@gmail.com>
2022-03-02 21:14:45 -0500

    added catch block to local storage hook

--- frontend/src/hooks/useLocalStorage.ts
+++ frontend/src/hooks/useLocalStorage.ts
@@ -1,20 +1,24 @@
 import { useEffect, useState } from 'react';
 
 export const getSavedValue = <T>(key: string, initialValue: T) => {
-  const savedValue =
-    localStorage.getItem(key) && JSON.parse(localStorage.getItem(key) || '');
-  if (savedValue) {
-    // in case there are any new keys added that aren't in user's local storage, check and add here to prevent errors
-    if (
-      typeof savedValue === 'object' &&
-      !Object.keys(initialValue).every((key) => savedValue[key])
-    ) {
-      return { ...initialValue, ...savedValue };
+  try {
+    const savedValue =
+      localStorage.getItem(key) && JSON.parse(localStorage.getItem(key) || '');
+    if (savedValue) {
+      // in case there are any new keys added that aren't in user's local storage, check and add here to prevent errors
+      if (
+        typeof savedValue === 'object' &&
+        !Object.keys(initialValue).every((key) => savedValue[key])
+      ) {
+        return { ...initialValue, ...savedValue };
+      }
+      return savedValue;
     }
-    return savedValue;
+    if (initialValue instanceof Function) return initialValue();
+    return initialValue;
+  } catch {
+    return initialValue;
   }
-  if (initialValue instanceof Function) return initialValue();
-  return initialValue;
 };
 
 const useLocalStorage = <T>(key: string, defaultValue?: T) => {
